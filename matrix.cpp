6e6c294c97b4ece53f7c3a8c10e5b42b9cb5dbb2
tymjames04 <tymjames04@gmail.com>
2025-10-28 20:08:24 -0500

    done through task5

--- matrix.cpp
+++ matrix.cpp
@@ -60,7 +60,7 @@ void mtxAdd(Matrix &mtx1, Matrix &mtx2){
         }
         tempmtx.mtx.push_back(row);
     }
-    cout << "Matrix 1 and Matrix 2 added together\n";
+    cout << "Matrix 1 and Matrix 2 added together:\n";
     tempmtx.print(); 
 }
 
@@ -79,7 +79,7 @@ void mtxMultiply(Matrix &mtx1, Matrix &mtx2){
         }
         outputMtx.mtx.push_back(row);
     }
-    cout << "Matrix 1 and Matrix 2 multiplied together\n";
+    cout << "Matrix 1 and Matrix 2 multiplied together:\n";
     outputMtx.print();
 }
 
@@ -97,6 +97,12 @@ void mtxDiagonalSum(Matrix &mtxz){
 }
 
 void mtxRowSwap(Matrix &mtxz, int row1, int row2){
+    Matrix outputMtx = mtxz;
+    vector<string> temp = outputMtx.mtx[row1];
+    outputMtx.mtx[row1] = outputMtx.mtx[row2];
+    outputMtx.mtx[row2] = temp;
+    cout << "Matrix with rows " << row1 << " and " << row2 << " swapped:\n";
+    outputMtx.print();
 }
 
 void mtxColumnSwap(Matrix &mtxz, int col1, int col2){
@@ -105,42 +111,75 @@ void mtxColumnSwap(Matrix &mtxz, int col1, int col2){
 void mtxUpdateRows(Matrix &mtxz, int row, int column){
 }
 
+bool boundchecker(Matrix &mtxz, int b1, int b2){
+    return((b1>=0 && b1<mtxz.mtx.size()) && (b2>=0 && b2<mtxz.mtx.size()));
+}
 
 int main(){
     Matrix mtx1;
     Matrix mtx2;
     string inputfile;
-    string selectedmtx;
     cout << "What is your input file name?: ";
     cin >> inputfile;
     load(inputfile, mtx1, mtx2);
-
+    //task1
     cout << "Vector 1\n";
     mtx1.print();
-
     cout << "Vector 2\n";
     mtx2.print();
 
+    //task2
     mtxAdd(mtx1,mtx2);
-    mtxMultiply(mtx1,mtx2);
 
-    cout << "Would you like to use mtx1 or mtx2 for the next operations?: ";
-    cin >> selectedmtx;
-    
-    if (selectedmtx == "mtx1"){
+    //task3
+    string task3mtx;
+    cout << "What would you like the first factor matrix to be? \"matrix1\" or \"matrix2\": ";
+    cin >> task3mtx;
+    if (task3mtx == "matrix1"){
+        mtxMultiply(mtx1,mtx2);
+    }else if (task3mtx == "matrix2"){
+        mtxMultiply(mtx2,mtx1);
+    }else{
+        cout << "you have inputted something invalid when selecting task3 matrix";
+        return(1);
+    }
+
+    //task4
+    string task4mtx;
+    cout << "Would you like to find the diagonal of \"matrix1\" or \"matrix2\"?: ";
+    cin >> task4mtx;
+    if (task4mtx == "matrix1"){
         mtxDiagonalSum(mtx1);
-        //mtxRowSwap(mtx1);
-        //mtxColumnSwap(mtx1);
-        //mtxUpdateRows(mtx1);
-    }
-    else if (selectedmtx == "mtx2"){
+    }else if (task4mtx == "matrix2"){
         mtxDiagonalSum(mtx2);
-       // mtxRowSwap(mtx2);
-        //mtxColumnSwap(mtx2);
-        //mtxUpdateRows(mtx2);
+    }else{
+        cout << "You have entered an invalid matrix option for task4";
+        return(1);
     }
-    else{
-        cout << "You have entered an invalid matrix option";
-        return 0;
-    }
+
+    //task5
+    string task5mtx;
+    int task5row1;
+    int task5row2;
+    cout << "Which matrix do you want to swap a row in? \"matrix1\" or \"matrix2\": ";
+    cin >> task5mtx;
+    cout << "What is the 1st row you want to swap?: ";
+    cin >> task5row1;
+    cout << "What is the 2nd row you want to swap?: ";
+    cin >> task5row2;
+    if (boundchecker){
+        if (task5mtx == "matrix1"){
+            mtxRowSwap(mtx1,task5row1,task5row2);
+        }else if (task5mtx == "matrix2"){
+            mtxRowSwap(mtx2,task5row1,task5row2);
+        }else{
+            cout << "You have entered something invalid for task 5";
+            return(1);
+        }
+    }else{
+        cout << "You have supplied invalid bounds that are out of range!";
+    }   
+
+    
+
 }
